eca2257f4e999526eadc47c33a76a0379e8c97c7
sitara-a <sitara.achar@gmail.com>
2024-11-18 15:49:44 -0600

    Add backend to project

---
+++ handyPerson/src/test/java/com/example/handyPerson/HandyPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.handyPerson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandyPersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
